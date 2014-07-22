7617cf48eee5a90f9f879ad80aa9450f374f3a2a
Eric Mountain <eric.stephen.mountain@gmail.com>
2014-07-22 23:16:20 +0200

    Session storage exbmple working.

--- js/main.js
+++ js/main.js
@@ -66,12 +66,60 @@ $(function() {
     // console.log(maxLineLength);
 
     // Bootstrap Angular
-    var taskMatrixApp = angular.module('taskMatrixApp', []);
+    var taskMatrixApp = angular.module('taskMatrixApp', ['ngRoute']);
 
-    taskMatrixApp.controller('taskMatrixCtrl', ['$scope', function($scope) {
-                $scope.text = "hi";
+    taskMatrixApp.factory('dataModelService', ['$rootScope', function ($rootScope) {
+
+        var service = {
+
+            model: {
+                text: ''
+            },
+
+            SaveState: function () {
+                sessionStorage.storageService = angular.toJson(service.model);
+                console.log("saved state: " + service.model.text);
+            },
+
+            RestoreState: function () {
+                service.model = angular.fromJson(sessionStorage.storageService);
+                console.log("restored state: " + service.model.text);
             }
-        ]);
+        }
+
+        $rootScope.$on("savestate", service.SaveState);
+        $rootScope.$on("restorestate", service.RestoreState);
+
+        return service;
+    }]);
+
+    taskMatrixApp.config(['$routeProvider', function($routeProvider) {
+        $routeProvider.when('', { controller: 'taskMatrixCtrl'})
+            .otherwise({ controller: 'taskMatrixCtrl'});
+    }]);
+
+    taskMatrixApp.controller('taskMatrixCtrl',
+                             ['$scope', '$rootScope', '$route', 'dataModelService', function($scope, $rootScope, $route, dataModelService) {
+                                 //$scope.text = "hi";
+                                 $scope.dataModelService = dataModelService;
+                                 
+                                 $scope.change = function() {
+                                     console.log("saving state: " + $scope.dataModelService.model.text);
+                                     //service.model.text = $scope.text;
+                                     $rootScope.$broadcast('savestate');
+                                 };
+
+                                 $scope.$on(
+                                     "$routeChangeSuccess",
+                                     function( $currentRoute, $previousRoute ){
+                                         console.log("restoring state");
+                                         $rootScope.$broadcast('restorestate');
+                                     }
+                                 );
+                                 
+                             }
+                             ]
+                            );
 
     angular.element(document).ready(function() {
         angular.bootstrap(document, ['taskMatrixApp']);
