c90b5061c1a836477e7898c5b5099651d0228b19
Eric Mountain <eric.stephen.mountain@gmail.com>
2014-08-10 22:15:39 +0200

    Add back quotes that are needed due to hyphens.

--- js/app.js
+++ js/app.js
@@ -3,25 +3,25 @@
 requirejs.config({
     baseUrl: "js/vendor",
     paths: {
-        app: "..",
-        jquery: "//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min",
-        angular: "//ajax.googleapis.com/ajax/libs/angularjs/1.3.0-beta.16/angular",
-        angular-route: "//ajax.googleapis.com/ajax/libs/angularjs/1.3.0-beta.16/angular-route",
-        angular-animate: "//ajax.googleapis.com/ajax/libs/angularjs/1.3.0-beta.16/angular-animate",
-        perfect-scrollbar: "perfect-scrollbar.min",
-        angular-perfect-scrollbar: "angular-perfect-scrollbar"
+        "app": "..",
+        "jquery": "//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min",
+        "angular": "//ajax.googleapis.com/ajax/libs/angularjs/1.3.0-beta.16/angular",
+        "angular-route": "//ajax.googleapis.com/ajax/libs/angularjs/1.3.0-beta.16/angular-route",
+        "angular-animate": "//ajax.googleapis.com/ajax/libs/angularjs/1.3.0-beta.16/angular-animate",
+        "perfect-scrollbar": "perfect-scrollbar.min",
+        "angular-perfect-scrollbar": "angular-perfect-scrollbar"
     },
     shim: {
-        angular-route: {
+        "angular-route": {
             deps: ['angular']
         },
-        angular-animate: {
+        "angular-animate": {
             deps: ['angular']
         },
-        perfect-scrollbar: {
+        "perfect-scrollbar": {
             deps: ['jquery']
         },
-        angular-perfect-scrollbar: {
+        "angular-perfect-scrollbar": {
             deps: ['perfect-scrollbar', 'angular']
         }
     }
