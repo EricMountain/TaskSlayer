873c4ec81e8e7bffc8df7069fbd279b9ae430907
Eric Mountain <eric.stephen.mountain@gmail.com>
2014-08-21 23:59:44 +0200

    Get rid of leading ".:" on required modules.

--- app/scripts/datastorage.js
+++ app/scripts/datastorage.js
@@ -2,7 +2,7 @@
 
 // Data storage layer
 
-define(['angular', './localstorage', './couchstorage', './schema'],
+define(['angular', 'localstorage', 'couchstorage', 'schema'],
     function() {
         'use strict';
 
